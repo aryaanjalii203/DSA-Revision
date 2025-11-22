7948e51bc5c30174a815b55a8c40de5842ce437a
aryaanjali203 <aryaanjali203@gmail.com>
2025-11-22 23:49:28 +0530

    LeetCode Ques 9

---
+++ LeetCode9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int n=x;
+        int rev=0;
+        while(n!=0){
+            int r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+        }
+        return rev==x;
+        
+    }
+}
